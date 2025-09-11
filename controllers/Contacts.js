c3f3ed07c513692294a5dcb24c0875e4db9e8ef9
P-Wall86 <sisterwall@gmail.com>
2025-09-11 01:52:09 -0300

    Contacts Part 2

--- controllers/Contacts.js
+++ controllers/Contacts.js
@@ -11,11 +11,62 @@ const getAll = async (req, res) => {
 
 const getSingle = async (req, res) => {
     const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('Contacts').find({_id: userId});
+    const result = await mongodb.getDatabase().db().collection('Contacts').find({ _id: userId });
     result.toArray().then((Contacts) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(Contacts[0]);
     })
 };
 
-module.exports = { getAll, getSingle };
+const createUser = async (req, res) => {
+    const Contact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+
+    const response = await mongodb.getDatabase().db().collection('Contacts').insertOne(Contact);
+
+    if (response.acknowledged) {
+        res.status(201).json({insertedId: response.insertedId});
+    } else {
+        res.status(500).json('Some error occurred while creating the Contact.');
+    }
+};
+
+const updateUser = async (req, res) => {
+    const ContactId = new ObjectId(req.params.id);
+    const Contact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+
+    const response = await mongodb.getDatabase().db().collection('Contacts').replaceOne(
+        { _id: ContactId },
+        Contact
+    );
+
+    if (response.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(404).json('Contact not found or no changes applied.');
+    }
+};
+
+const deleteUser = async (req, res) => {
+    const ContactId = new ObjectId(req.params.id);
+    const response = await mongodb.getDatabase().db().collection('Contacts').deleteOne({ _id: ContactId });
+
+    if (response.deletedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(404).json('Contact not found.');
+    }
+};
+
+module.exports = { getAll, getSingle, createUser, updateUser, deleteUser };
--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
 const app = express();
 
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerDocument = require('./swagger.json');
+
 const port = process.env.PORT; 
 
-app.use ('/', require('./routes'));
+app
+    .use(cors())
+    .use(express.json())
+    .use(express.urlencoded({ extended: true }))
+    .use(bodyParser.json())
+    .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+    .use ('/', require('./routes'));
+
 
 mongodb.initDb((err) => {
     if (err) {
